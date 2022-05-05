96dcd586ce9c4245be6bf068fab33105c414fb49
Thomas Tran Dinh <tdt7384@gmail.com>
2022-05-05 12:27:50 -0400

    changement mineurs

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,11 @@
-"use strict"
+"use strict";
 
 exports.ifTokenExists = (req, res, next) => {
 
     const token = res.app.locals.apiToken;
 
-    if(!token)
-        res.render("error", {title: "Interdit", eMessage: "Vous n'avez pas l'access à cette page"});
+    if (!token)
+        res.render("error", { title: "Interdit", eMessage: "Vous n'avez pas l'access à cette page" });
     else
         next();
 };
